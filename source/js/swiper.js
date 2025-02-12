972ef32609f5acb36d489540bed98ab5c821da6c
Danila <daniilpetrov776@gmail.com>
2025-02-12 21:45:20 +0300

    reviews

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -2,10 +2,12 @@ import { initHeroSwiper } from './hero-swiper';
 import { initProgramsSwiper } from './programs-swiper';
 import { initNewsSwiper } from './news-swiper';
 import { handleFaqControls } from './accordeon';
+import { initReviewsSwiper } from './reviews-swiper';
 
 export const initSwipers = () => {
   initHeroSwiper();
   initProgramsSwiper();
   initNewsSwiper();
   handleFaqControls();
+  initReviewsSwiper();
 };
